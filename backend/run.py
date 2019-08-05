69e81cbd7e6eeac6092c22c82f41cc4512513929
Wout De Puysseleir <woutdp@gmail.com>
2019-08-04 21:10:12 -0700

    Use single quotes

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
     return "Hello World!"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0')
